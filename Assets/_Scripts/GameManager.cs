b0c8043112e8392c2261a97b32a63ada82844628
Marcelo Cesário Miguel <marcelocm8@al.insper.edu.br>
2022-05-31 10:29:40 -0300

    MENU

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -12,6 +12,7 @@ public class GameManager
    public double distance;
    public bool passou;
    public int fase;
+   public int scene_index;
    public static GameManager GetInstance()
    {
        
@@ -24,21 +25,38 @@ public class GameManager
    }
    private GameManager()
    {
-       SceneManager.LoadScene(0);
+       SceneManager.LoadScene("Menu");
        passou = false;
        distance = 10;
-       gameState = GameState.GAME;
+       scene_index = 1;
+       gameState = GameState.MENU;
    }
 
    public void NextScene(){
-       distance = 10;
-        SceneManager.LoadScene(1);
-        passou = false;
+       
+       if (scene_index + 1 <SceneManager.sceneCountInBuildSettings){
+           scene_index +=1;
+           SceneManager.LoadScene(scene_index);
+           passou = false;
+           distance = 10;
+       }
+       else{
+           SceneManager.LoadScene("Menu");
+           passou = false;
+           distance = 10;
+       }
+
+   }
+
+    public void GoToScene(string scene_name){
+        SceneManager.LoadScene(scene_name);
+    }
+       
 //        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
 //         if (SceneManager.sceneCount > nextSceneIndex)
 //         {
 //             SceneManager.LoadScene(nextSceneIndex);
 //         }
 
-   }
+   
 }
