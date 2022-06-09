5c0113c05fc223a16b1acda68f05e6c7bd9a5663
Marcelo Cesário Miguel <marcelocm8@al.insper.edu.br>
2022-06-09 15:46:18 -0300

    timer and endgame

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float TimeLeft;
+    public bool TimerOn = false;
+
+    public Text TimerTxt;
+   GameManager gm;
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+        TimerOn = true;
+    }
+
+    void Update()
+    {
+        if(TimerOn)
+        {
+            if(TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+                updateTimer(TimeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is UP!");
+                TimeLeft = 0;
+                TimerOn = false;
+                gm.GoToScene("Lose");
+            }
+        }
+    }
+
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float seconds = Mathf.FloorToInt(currentTime);
+
+        TimerTxt.text = string.Format("{0:00}", seconds);
+    }
+
+}
